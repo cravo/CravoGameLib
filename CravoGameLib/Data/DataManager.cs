9d4c106d4fe8e44e8408c1766dbbb69d172bf4fa
Matt Craven <matthew.craven@btinternet.com>
2015-05-31 12:11:39 +0100

    Initial files, doesn't do anything yet.

---
+++ CravoGameLib/Data/DataManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CravoGameLib.Data
+{
+    class DataManager
+    {
+        public DataManager(string rootPath)
+        {
+
+        }
+
+        public T Load<T>(string filename)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
