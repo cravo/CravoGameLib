811c5873288e067c4fa6669c54ad39da242a4e3a
Matt Craven <matthew.craven@btinternet.com>
2015-06-01 23:29:39 +0100

    Added untracked files

---
+++ CravoGameLib/Data/DataProcessor.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CravoGameLib.Data
+{
+    /// <summary>
+    /// DataProcessor base class.  In general you should derive from DataProcessor<T> not this class
+    /// </summary>
+    public abstract class DataProcessor
+    {
+        public Type DataType { get; private set; }
+
+        protected DataProcessor(Type type)
+        {
+            DataType = type;
+        }
+
+        protected internal abstract object ProcessData(DataReader reader);
+
+        public abstract string GetSupportedFilename(string filename);
+
+        protected internal string GetSupportedFilename(string filename, string [] supportedExtensions)
+        {
+            string result = filename;
+
+            foreach ( string extension in supportedExtensions )
+            {
+                string fullPath = filename + "." + extension;
+                if (File.Exists(fullPath))
+                {
+                    result = fullPath;
+                }
+            }
+
+            return result;
+        }
+    }
+
+    /// <summary>
+    /// DataProcessor base class templated on type.  Derive all data processors from this.
+    /// </summary>
+    /// <typeparam name="T">The type the data processor creates</typeparam>
+    public abstract class DataProcessor<T> : DataProcessor
+    {
+        protected DataProcessor() : base(typeof(T))
+        {
+
+        }
+
+        protected internal override object ProcessData(DataReader reader)
+        {
+            return this.ProcessData(reader, default(T));
+        }
+
+        protected internal abstract T ProcessData(DataReader reader, T instance);
+    }
+}
---
+++ CravoGameLib/Data/Processor/Texture2DProcessor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CravoGameLib.Data.Processor
+{
+    /// <summary>
+    /// Data processor for 2D textures
+    /// </summary>
+    class Texture2DProcessor : DataProcessor<Texture2D>
+    {
+        private string[] SupportedExtensions =
+        {
+            "png",
+            "jpg",
+            "bmp"
+        };
+
+        public override string GetSupportedFilename(string filename)
+        {
+            return GetSupportedFilename(filename, SupportedExtensions);
+        }
+
+        protected internal override Texture2D ProcessData(DataReader reader, Texture2D instance)
+        {
+            Texture2D texture = Texture2D.FromStream(reader.GraphicsDevice, reader.Stream);
+
+            return texture;
+        }
+    }
+}
