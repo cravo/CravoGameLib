5a21e3bc0db1973da499ca29067ce590292944b0
Matt Craven <matthew.craven@btinternet.com>
2015-06-01 23:28:11 +0100

    Documentation for the new classes.

--- CravoGameLib/Data/DataManager.cs
+++ CravoGameLib/Data/DataManager.cs
@@ -8,6 +8,17 @@ using System.Threading.Tasks;
 
 namespace CravoGameLib.Data
 {
+    /// <summary>
+    /// The DataManager class allows you to load data for various different types.
+    /// 
+    /// It serves as a replacement for the MonoGame content manager since their content
+    /// pipeline only supports 64-bit and I wanted something that would work on 32-bit
+    /// systems too.
+    /// 
+    /// It maintains a cache of loaded objects, so if you request an object more than once
+    /// it will be returned instantly from the cache rather than re-loading it.
+    /// 
+    /// </summary>
     public class DataManager
     {
         Dictionary<string, object> CachedData;
@@ -15,6 +26,12 @@ namespace CravoGameLib.Data
         string RootPath;
         GraphicsDevice GraphicsDevice;
 
+        /// <summary>
+        /// Create a new DataManager for loading data.  It's perfectly fine to have
+        /// multiple data managers if needed
+        /// </summary>
+        /// <param name="rootPath">The path below which all your data lives.</param>
+        /// <param name="device">The MonoGame GraphicsDevice (since it may be required by certain content processors)</param>
         public DataManager(string rootPath, GraphicsDevice device)
         {
             this.GraphicsDevice = device;
@@ -31,16 +48,25 @@ namespace CravoGameLib.Data
             InitialiseDataProcessors();
         }
 
+        /// <summary>
+        /// Loads an object from data
+        /// </summary>
+        /// <typeparam name="T">The type of the object.  Throws an exception if no processor is available for that type</typeparam>
+        /// <param name="filename">The filename of the file to load, relative to the root path passed to the constructor.
+        /// Do not specify an extension - the relevant data processor will sort that out for you.</param>
+        /// <returns>The loded object</returns>
         public T Load<T>(string filename)
         {
             object result = null;
             filename = RootPath + filename;
 
+            // First check the cache and return from there if it exists
             bool cached = CachedData.TryGetValue(filename, out result);
 
             if ( !cached )
             {
                 DataProcessor processor = GetProcessor(typeof(T));
+
                 filename = processor.GetSupportedFilename(filename);
 
                 using (StreamReader reader = new StreamReader(filename))
@@ -52,6 +78,11 @@ namespace CravoGameLib.Data
             return (T)result;
         }
 
+        /// <summary>
+        /// Get the data processor for the given type
+        /// </summary>
+        /// <param name="type">The type a processor is required for.</param>
+        /// <returns>The relevant data processor.  Throws an exception if one couldn't be found.</returns>
         private DataProcessor GetProcessor(Type type)
         {
             DataProcessor result = null;
--- CravoGameLib/Data/DataReader.cs
+++ CravoGameLib/Data/DataReader.cs
@@ -8,6 +8,11 @@ using System.Threading.Tasks;
 
 namespace CravoGameLib.Data
 {
+    /// <summary>
+    /// Data to read is passed to the DataProcessor via this class.
+    /// It encapsulates the data stream plus any other information the processors
+    /// may need in order to process their data.
+    /// </summary>
     public class DataReader
     {
         public GraphicsDevice GraphicsDevice { get; private set; }
