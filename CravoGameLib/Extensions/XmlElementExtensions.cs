c4f0e481312d5fa0b0fae3c8dab7deb6df04ab63
Matt Craven <matthew.craven@btinternet.com>
2015-06-11 21:31:51 +0100

    Beginnings of tile map class. Added XmlElementExtensions. Added TileEd sample map to test app.

---
+++ CravoGameLib/Extensions/XmlElementExtensions.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace CravoGameLib.Extensions
+{
+    /// <summary>
+    /// Extension methods for XmlElement
+    /// </summary>
+    public static class XmlElementExtensions
+    {
+        /// <summary>
+        /// If an element has the specified attribute, return it as a string, otherwise return the default
+        /// </summary>
+        /// <param name="element">The element to check</param>
+        /// <param name="attribute">The attribute name</param>
+        /// <param name="defaultValue">The default value if the attribute doesn't exist</param>
+        /// <returns>The attribute value, or the default if the attribute didn't exist</returns>
+        public static string TryGetString(this XmlElement element, string attribute, string defaultValue)
+        {
+            if(element.HasAttribute(attribute))
+            {
+                return element.GetAttribute(attribute);
+            }
+            else
+            {
+                return defaultValue;
+            }
+        }
+
+        /// <summary>
+        /// If an element has the specified attribute, return it as an int, otherwise return the default
+        /// </summary>
+        /// <param name="element">The element to check</param>
+        /// <param name="attribute">The attribute name</param>
+        /// <param name="defaultValue">The default value if the attribute doesn't exist</param>
+        /// <returns>The attribute value, or the default if the attribute didn't exist</returns>
+        public static int TryGetInt(this XmlElement element, string attribute, int defaultValue)
+        {
+            if(element.HasAttribute(attribute))
+            {
+                return Convert.ToInt32(element.GetAttribute(attribute));
+            }
+            else
+            {
+                return defaultValue;
+            }
+        }
+    }
+}
---
+++ CravoGameLib/TileMap/Map.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using CravoGameLib.Extensions;
+
+namespace CravoGameLib.TileMap
+{
+    public class Map
+    {
+        public enum OrientationType
+        {
+            Orthogonal,
+            Isometric,
+            Staggered,
+        }
+
+        public enum RenderOrderType
+        {
+            RightDown,
+            RightUp,
+            LeftDown,
+            LeftUp,
+        }
+
+        private string Version { get; set; }
+        public OrientationType Orientation { get; private set; }
+        public int WidthInTiles { get; private set; }
+        public int HeightInTiles { get; private set; }
+        public int TileWidth { get; private set; }
+        public int TileHeight { get; private set; }
+        public Color BackgroundColour { get; set; }
+        private int NextObjectID { get; set; }
+
+        public Map()
+        {
+            InitDefaults();
+        }
+
+        public Map(string file)
+        {
+            InitDefaults();
+
+            Load(file);
+        }
+
+        private void InitDefaults()
+        {
+            Version = "1.0";
+            Orientation = OrientationType.Orthogonal;
+            WidthInTiles = 1;
+            HeightInTiles = 1;
+            TileWidth = 1;
+            TileHeight = 1;
+            BackgroundColour = Color.Black;
+            NextObjectID = 1;
+        }
+
+        public void Load(string file)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(file);
+
+            foreach (XmlElement mapElement in doc.GetElementsByTagName("map"))
+            {
+                Version = mapElement.TryGetString("version", Version);
+
+                // Parse orientation from string to enum, ensuring it's Uppercase first letter, lowercase the rest
+                string orientationString = mapElement.TryGetString("orientation", Enum.GetName(typeof(OrientationType), Orientation));
+                orientationString.ToLower();
+                orientationString = char.ToUpper(orientationString[0]) + orientationString.Substring(1);
+                Orientation = (OrientationType)Enum.Parse(typeof(OrientationType), orientationString);
+
+                WidthInTiles = mapElement.TryGetInt("width", WidthInTiles);
+                HeightInTiles = mapElement.TryGetInt("height", HeightInTiles);
+                TileWidth = mapElement.TryGetInt("tilewidth", TileWidth);
+                TileHeight = mapElement.TryGetInt("tileheight", TileHeight);
+
+                //todo: Background Colour, leave unsupported for now.
+                //todo: RenderOrder, leave unsupported for now.
+            }
+        }
+    }
+}
